5d86604aa73a9d1e2794de99ec8dde2fbe77b4f5
BoQuan <mo791206@gmail.com>
2021-08-05 19:19:22 +0800

    新增河流shader & Player移動

---
+++ PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float forwardForce = 100f;
+
+
+
+    [Header("Player Control")]
+    [SerializeField] private bool turnLeft;
+    [SerializeField] private bool turnRight;
+    [SerializeField] private float turnSpeed = 100f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            turnLeft = true;
+        if (Input.GetKeyUp(KeyCode.A))
+            turnLeft = false;
+      
+        if (Input.GetKeyDown(KeyCode.D))
+            turnRight = true;
+        if (Input.GetKeyUp(KeyCode.D))
+            turnRight = false;
+    }
+
+
+    private void FixedUpdate()
+    {
+        
+
+        Movement();
+
+
+    }
+
+    public void Movement()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.Force);
+        if (turnLeft)
+        {
+            rb.AddForce(turnSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (turnRight)
+        {
+            rb.AddForce(-turnSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+
+
+}
